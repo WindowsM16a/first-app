8b9445dc7ad9bab3be6cd6a863630185f4875bcd
WindowsM16a <wuvershayne@gmail.com>
2024-10-03 20:00:18 +0000

    react is an angry framework jeez

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const name = 'John';
+  const x = 10;
+  const y = 5;
+  const names = ['Brad', 'Harry', 'Jo', 'Sara'];
+  const loggedIn = true;
 
-function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <div className="text-5xl">App</div>
+      <p>{name}</p>
+      <p>
+        the sum of {x} and {y} is {x+y}
       </p>
+      <ul>
+        {names.map((name, index) => (  
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn ? <h1>hi bitch</h1> : <h1>hi nigga</h1> }
     </>
   )
-}
+};
 
-export default App
+export default App;
+
