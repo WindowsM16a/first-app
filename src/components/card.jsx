2b3059e778e91805a4a99eebad4375aee7bb5ff2
WindowsM16a <wuvershayne@gmail.com>
2024-10-24 02:25:21 +0000

    damn these bugs

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,5 @@
-const Card = ({ children, bg = "bg-gray-100" }) => {
-  return <div className={`${bg} p-5 rounded-lg shadow-md`}>{children}</div>;
+const card = ({ children, bg = "bg-gray-100" }) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
-export default Card;
+export default card;
